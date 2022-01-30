dbcae38bb8047562416e25f57ae4f093e213b5ae
azurealstn <azurealstn@naver.com>
2022-01-30 14:42:33 +0900

    다대일 매핑관계 프로젝트 생성

---
+++ src/main/java/com/azurealstn/springbootcodeweb2/SpringbootCodeWeb2Application.java
@@ -0,0 +1,13 @@
+package com.azurealstn.springbootcodeweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCodeWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCodeWeb2Application.class, args);
+	}
+
+}
